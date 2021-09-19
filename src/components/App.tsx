cdbf93b0a18602737fb9140d0442b6d786be8e6f
Ing. Martin Patyk <martin@patyk.cz>
2021-09-19 17:58:37 +0200

    created routing between first two components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import CompanyIndex from './company/CompanyIndex';
 import CountryIndex from './country/CountryIndex';
+import {BrowserRouter as Router, Link, Switch, Route, Redirect} from "react-router-dom";
+import { Nav } from 'react-bootstrap';
 
 export default class App extends React.Component<any, any> {
     render = () => {
         return (
-            <div>
+            <Router>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to={"/company"} className="nav-link">Companies</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={"/country"} className="nav-link">Country</Link>
+                        </li>
+                    </ul>
+                </nav>
                 <h1>Hello world!</h1>
-                <CountryIndex/>
-                <hr />
-                <CompanyIndex/>
-            </div>
+                <Switch>
+                    <Route path={"/company"} component={CompanyIndex} />
+                    <Route path={"/country"} component={CountryIndex} />
+                </Switch>
+                <Redirect from="/" to="/company"/>
+            </Router>
+
         );
     }
 }
